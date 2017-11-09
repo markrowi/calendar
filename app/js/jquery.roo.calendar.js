5786b6f2316df4ca13c2f99adc4ec072e504836a
Rowi Dizon <rowi.dizon@nuworks.ph>
2017-11-09 13:59:41 +0800

    col resize

--- app/js/jquery.roo.calendar.js
+++ app/js/jquery.roo.calendar.js
@@ -49,35 +49,37 @@
                 $('.event').removeClass('focused');
                 $(this).addClass('focused');
                 e.stopPropagation();
-            })
+            });
             
             $(this.element).on('click','.rc-col-header', function(){
                 $('.rc-col-header').removeClass('focused');
                 $(this).addClass('focused');
-            })
+            });
 
             $(this.element).on('click','.remove-event', function(){
                 $(this).closest('.event').remove();
                 self.eventResizeWidth();
-            })
+            });
 
            
             $(this.element).on('click', '.event', function(e){
                 e.stopPropagation();
-            })
+            });
+
             $(this.element).on('click', '.rc-event-list', function(e){
                 $('.event').removeClass('focused');
-            })
+            });
 
             $(this.element).on('dblclick', '.event', function(e){
                 e.stopPropagation();
-            })
+            });
+
             $(this.element).on('dblclick', '.rc-event-list', function(e){
                 const y = Math.floor(e.offsetY/self.settings.cellHeight);
                 const x = Math.floor(e.offsetX/self.settings.cellWidth);
                 // console.log(Math.floor(e.offsetY/self.settings.cellHeight), Math.floor(e.offsetX/self.settings.cellWidth))
-                self.add(x, y, 1)
-            })
+                self.add(x, y, 1);
+            });
             
             
             
@@ -155,7 +157,7 @@
             $table.appendTo($rcContent)
 
             $(`<div class="rc-header-content"></div>`).appendTo(this.element)
-            $rcContent.appendTo(this.element)
+            $rcContent.appendTo(this.element);
             var x = Math.ceil($('.rc-event-container').width()/ (this.settings.resource || this.settings.days).length);
             self.settings.cellWidth = x;
           
@@ -241,53 +243,149 @@
             return {start:start, end:end};
         },
         eventResizeWidth:function(){
+            
             const self = this;
             const columns = self.settings.resource || self.settings.days;
             let counter = 0;
             $('.event', this.element).attr('data-group-x',"");
             for(colI = 0; colI < columns.length; colI++){
+                self.sortEvents($(`.event[data-pos-x="${colI}"]`, self.element));
                 $eventGroup = $(`.event[data-pos-x="${colI}"]`, self.element);
-                $.each($eventGroup, function(pEventIndex, parentEvent){
-                    const pSE = self.getEventSE(parentEvent);
-                    counter++;
-                    $.each($eventGroup, function(cEventIndex, childEvent){
-                        const $cEvent = $(childEvent);
-                        const cSE = self.getEventSE(childEvent);
-                        $cEvent.css('width',self.settings.cellWidth- 3) + 'px';
-                        if(childEvent != parentEvent){
-                                if((pSE.start >=cSE.start && pSE.end<=cSE.end) || (pSE.start<=cSE.start && pSE.end>=cSE.end) || (pSE.end>=cSE.start && pSE.start<=cSE.end)){
-                                    // console.log("Parent :", pSE.start,pSE.end, "Child :", cSE.start , cSE.end, counter)
-                                    // group.push(ev);
-                                    if($cEvent.attr('data-group-x')){
-                                        $cEvent.attr('data-group-x', 'group-' + counter)
-                                    }
+                this.updateEvents($eventGroup);
+              
+                // $.each($eventGroup, function(pEventIndex, parentEvent){
+                //     console.log($eventGroup)
+                //     const pSE = self.getEventSE(parentEvent);
+                //     counter++;
+                //     $.each($eventGroup, function(cEventIndex, childEvent){
+                //         const $cEvent = $(childEvent);
+                //         const cSE = self.getEventSE(childEvent);
+                //         $cEvent.css('width',self.settings.cellWidth- 3) + 'px';
+                //         if(childEvent != parentEvent){
+                //                 if((pSE.start >=cSE.start && pSE.end<=cSE.end) || (pSE.start<=cSE.start && pSE.end>=cSE.end) || (pSE.end>=cSE.start && pSE.start<=cSE.end)){
+                //                     // console.log("Parent :", pSE.start,pSE.end, "Child :", cSE.start , cSE.end, counter)
+                //                     // group.push(ev);
+                //                     if($cEvent.attr('data-group-x')){
+                //                         $cEvent.attr('data-group-x', 'group-' + counter)
+                //                     }
                                     
-                                    
-                                }
-                        }else{
-                            $cEvent.attr('data-group-x', 'group-' + counter)
-                            // console.log('same')
-                        }
-                     })
-                })
+                //                 }
+                //         }else{
+                            
+                //             $cEvent.attr('data-group-x', 'group-' + counter)
+                //             // console.log('same')
+                //         }
+                //      })
+                // })
             }
 
-            $.each($('.event'), function(i,pev){
-                const group = $(pev).attr('data-group-x');
-                const $groupEvent = $(`.event[data-group-x="${group}"]`);
-                let $pev = $(pev);
+            // $.each($('.event'), function(i,pev){
+            //     const group = $(pev).attr('data-group-x');
+            //     const $groupEvent = $(`.event[data-group-x="${group}"]`);
+            //     let $pev = $(pev);
                
-                const colCount = $groupEvent.length;
-                const colWidth = Math.floor(self.settings.cellWidth/colCount);
-                $groupEvent.each(function(ind, ev){
-                   const $ev = $(ev);
-                    $ev.css({
-                            'width':(colWidth - 3) + 'px',
-                            'margin-left': (colWidth * ind) + 'px'
-                        });
-                })
+            //     const colCount = $groupEvent.length;
+            //     const colWidth = Math.floor(self.settings.cellWidth/colCount);
+            //     $groupEvent.each(function(ind, ev){
+            //        const $ev = $(ev);
+            //         $ev.css({
+            //                 'width':(colWidth - 3) + 'px',
+            //                 'margin-left': (colWidth * ind) + 'px'
+            //             });
+            //     })
               
 
+            // })
+        },
+        
+        collidesWith: function (a, b) {
+            // console.log(a,b);
+            return a.end > b.start && a.start < b.end;
+        },
+        checkCollision: function (eventArr) {
+            for (var i = 0; i < eventArr.length; i++) {
+            eventArr[i].cols = [];
+            eventArr[i].colsBefore=[];
+            for (var j = 0; j < eventArr.length; j++) {
+                if (this.collidesWith(this.getEventSE(eventArr[i]), this.getEventSE((eventArr[j])))) {
+                eventArr[i].cols.push(j);
+                if(i>j) eventArr[i].colsBefore.push(j); //also list which of the conflicts came before
+                }
+            }
+            }
+            return eventArr;
+        },
+        updateEvents:function(eventArr) {
+            updateEvents = $('.events')
+            eventArr = this.checkCollision(eventArr);
+            // console.log('eventArr',eventArr);
+            var arr=eventArr.slice(0); //clone the array
+            for(var i=0; i<arr.length; i++){
+                var el=arr[i];
+            // el.color = getRandomColor();
+            // el.height = (el.end - el.start) * 2 + 'px';
+            // el.top = (el.start) * 2 + 'px';
+            
+            if(i>0 && el.colsBefore.length>0){ //check column if not the first event and the event has collisions with prior events
+                if(arr[i-1].column>0){ //if previous event wasn't in the first column, there may be space to the left of it
+                    for(var j=0;j<arr[i-1].column;j++){ //look through all the columns to the left of the previous event
+                    if(el.colsBefore.indexOf(i-(j+2))===-1){ //the current event doesn't collide with the event being checked...
+                        el.column=arr[i-(j+2)].column; //...and can be put in the same column as it
+                    }
+                }
+                if(typeof el.column==='undefined') el.column=arr[i-1].column+1; //if there wasn't any free space, but it ito the right of the previous event
+                }else{
+                    var column=0;
+                for(var j=0;j<el.colsBefore.length;j++){ //go through each column to see where's space...
+                    if(arr[el.colsBefore[el.colsBefore.length-1-j]].column==column) column++;
+                }
+                    el.column=column;
+                }
+            }else el.column=0;
+            }
+            //We need the column for every event before we can determine the appropriate width and left-position, so this is in a different for-loop:
+            for(var i=0; i<arr.length; i++){
+                arr[i].totalColumns=0;
+                if(arr[i].cols.length>1){ //if event collides
+                var conflictGroup=[]; //store here each column in the current event group
+                var conflictingColumns=[]; //and here the column of each of the events in the group
+                addConflictsToGroup(arr[i]);
+                function addConflictsToGroup(a){
+                for(k=0;k<a.cols.length;k++){
+                    if(conflictGroup.indexOf(a.cols[k])===-1){ //don't add same event twice to avoid infinite loop
+                    conflictGroup.push(a.cols[k]);
+                    conflictingColumns.push(arr[a.cols[k]].column);
+                    addConflictsToGroup(arr[a.cols[k]]); //check also the events this event conflicts with
+                    }
+                }
+                }
+                arr[i].totalColumns=Math.max.apply(null, conflictingColumns); //set the greatest value as number of columns
+            }
+            var width = (Math.floor(this.settings.cellWidth -3) /(arr[i].totalColumns+1))+'px';
+            var left = (Math.floor(this.settings.cellWidth - 3)/(arr[i].totalColumns+1)*arr[i].column)+'px';
+        
+            $(arr[i]).css("width",width);
+            $(arr[i]).css("margin-left",left);
+            console.log(width, left)
+            }
+            return arr;
+        },
+        sortEvents($events){
+
+            // return $events.sort(function(a, b) {
+            //     return a.offsetTop - b.offsetTop;
+            //   });
+
+            $events.each(function(i, event){
+                $events.each(function(i, cEvent){
+                    if(event.offsetTop<cEvent.offsetTop){
+                        $(event).after(cEvent)
+                    }else if(event.offsetTop>cEvent.offsetTop){
+                         $(event).before(cEvent)
+                    }else{
+
+                    }
+                })
             })
         },
         printDetails:function(event){
@@ -330,7 +428,7 @@
                 },
                 stop:function(){
                     self.eventResizeWidth();
-                    console.log('drag stop')
+                    // console.log('drag stop')
                 }
             }).resizable({
                 grid:[0,self.settings.cellHeight],
